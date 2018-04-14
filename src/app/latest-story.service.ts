f1f187014929cb0e0b471d2c3d1a2cd4108e0fbd
durai4585 <durai.chandra45@gmail.com>
2018-04-14 15:01:31 -0700

    updated vertx service url for rss feed

--- src/app/latest-story.service.ts
+++ src/app/latest-story.service.ts
@@ -2,64 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { forkJoin } from "rxjs/observable/forkJoin";
-import { parseString } from 'xml2js';
-import * as xml2js from "xml2js";
-//import {map}  from 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class LatestStoryService {
-    constructor(private _http: HttpClient) { }
+    constructor( private _http: HttpClient ) { }
 
-    getScreenShots(page: number): Observable<any[]> {
-        return forkJoin([
+    getScreenShots( page: number ): Observable<any[]> {
+        return forkJoin( [
 
-            //-------------------dribbble------------------//
-            //this._http.get('https://api.dribbble.com/v1/shots?access_token=0b8f0bbf2352be625a2e69211592aed5003b101d7fc01c40d2928e1e3e39bfe1&page=' + page),
+            this._http.get<any[]>( 'http://buxdio.com/api/design/page/' + page )
 
-            //-------------------designernews------------------//
-            this._http.get<any[]>('https://www.designernews.co/?format=rss', {
-                headers: new HttpHeaders()
-                    .append('Content-Type', 'application/x-www-form-urlencoded')
-                    .append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
-                    .append('Access-Control-Allow-Origin', '*')
-                    .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method")
-                , responseType: 'text'
-            }),
-
-            //-------------------producthunt------------------//
-            this._http.get<any[]>('https://www.producthunt.com/feed?category=undefined', {
-                headers: new HttpHeaders()
-                    .append('Content-Type', 'application/x-www-form-urlencoded')
-                    .append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
-                    .append('Access-Control-Allow-Origin', '*')
-                    .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method")
-                , responseType: 'text'
-            }),
-
-
-            //-------------------behance------------------//
-            this._http.get<any[]>('https://www.behance.net/v2/projects?sort=published_date&field=132&content=project&api_key=OGWw0DdyHFb4h9zjKIXAFNKvPG1iJRJ6'),
-
-
-            //-------------------medium------------------//
-            //this._http.get<any[]>('https://medium.com/feed/@uxplanet')
-            //   or      https://medium.com/feed/topic/digital-design
-
-            //-------------------thenextweb------------------//
-            //this._http.get<any[]>('https://thenextweb.com/feed')
-
-
-            //-------------------pixabay------------------//
-            //https://pixabay.com/api/?key=8459865-2d66ad6fa313800389166bfcb
-            //https://pixabay.com/api/?key=8459865-2d66ad6fa313800389166bfcb&q=yellow+flowers&image_type=photo
-
-
-            //Referance:
-            //https://usepanda.com/app/#/
-            //https://www.prototypr.io/home/
-            //https://news.ycombinator.com/news
-
-
-        ]);
+        ] );
     }
 }
--- src/app/latest-story/latest-story.component.ts
+++ src/app/latest-story/latest-story.component.ts
@@ -8,16 +8,16 @@ import { forkJoin } from "rxjs/observable/forkJoin";
 import { LatestStoryService } from "../latest-story.service";
 import { parseString } from 'xml2js';
 
-@Component({
+@Component( {
     selector: 'app-latest-story',
     templateUrl: './latest-story.component.html',
     styleUrls: ['./latest-story.component.css']
-})
+} )
 export class LatestStoryComponent implements OnInit {
 
     posts = [];
     pageNumber = 1;
-    constructor(private http: HttpClient, private latestStoryService: LatestStoryService) { }
+    constructor( private http: HttpClient, private latestStoryService: LatestStoryService ) { }
 
     ngOnInit() {
         this.getData();
@@ -28,11 +28,11 @@ export class LatestStoryComponent implements OnInit {
     }
 
 
-    predicateBy(prop) {
-        return function(a, b) {
-            if (a[prop] > b[prop]) {
+    predicateBy( prop ) {
+        return function( a, b ) {
+            if ( a[prop] > b[prop] ) {
                 return 1;
-            } else if (a[prop] < b[prop]) {
+            } else if ( a[prop] < b[prop] ) {
                 return -1;
             }
             return 0;
@@ -40,82 +40,41 @@ export class LatestStoryComponent implements OnInit {
     }
 
     getData() {
-        this.latestStoryService.getScreenShots(this.pageNumber).subscribe(results => {
+        this.latestStoryService.getScreenShots( this.pageNumber ).subscribe( results => {
             let list = [];
 
-            //----------------------dribbble------------------------------//
 
-            results[0].forEach(element => {
-                list.push({
+            // console.log("RSS :: "+results[0]);
+
+            let temp = [];
+
+            //console.log( results[0] );
+
+            temp = JSON.parse( results[0] );
+
+            temp.forEach( element => {
+
+                console.log( element );
+                list.push( {
                     "title": element.title,
-                    "image": element.images.normal,
-                    "url": element.html_url,
-                    "views_count": element.views_count,
-                    "source": "dribbble"
-                });
-            });
+                    "url": element.link,
+                    "source": element.host,
+                    "image": element.image
+                } );
 
-            //----------------------designernews------------------------------//
-
-            parseString(results[0], function(err, result) {
-                //console.log(result.rss.channel[0].item);
-                result.forEach(element => {
-                    list.push({
-                        "title": element.title,
-                        "url": element.link,
-                    });
-                    //console.log(element.title[0]);
-                });
-            });
-
-            //----------------------producthunt------------------------------//
-
-            parseString(results[2], function(err, result) {
-                 console.log(result);
-                result.feed.entry.forEach(element => {
-                    list.push({
-                        "title": element.title,
-                        "url": element.link,
-                        "source": "producthunt",
-                        "image": 'assets/images/ph.png',
-                    });
-                    //console.log(element.title[0]);
-                });
-            });
-
-            //----------------------behance------------------------------//
-
-            results[3].projects.forEach(element => {
-                //console.log("element : " + element.name);
-                list.push({
-                    "title": element.name,
-                    "image": element.covers.original,
-                    "url": element.url,
-                    "source": "behance"
-                });
-            });
+            } );
 
 
-            //----------------------medium------------------------------//
-            //console.log(results[4]);
+            list.forEach( element => {
 
-            //let JSON_HIJACKING_PREFIX = '])}while(1);</x>';
-
-            //console.log(JSON.parse(results[4].replace(JSON_HIJACKING_PREFIX, '')));
-
-            //----------------------thenextweb------------------------------//
-            // console.log(results[5]);
-
-            list.forEach(element => {
-
-                this.posts.push(element);
-            });
-            this.posts.sort(this.predicateBy('title'));
+                this.posts.push( element );
+            } );
+            this.posts.sort( this.predicateBy( 'title' ) );
 
 
 
-            console.log(this.pageNumber);
-        });
+            console.log( this.pageNumber );
+        } );
     }
 
 
